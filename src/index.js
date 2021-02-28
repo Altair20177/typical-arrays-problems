5f4059d5bc80f6b0ae57b2dcb1b57e4672838bb9
Maksim <78382480+Altair20177@users.noreply.github.com>
2021-02-28 22:57:53 +0300

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minArr = array[0];
+  for (let i = 0; i < array.size;i++)
+  {
+     if(array[i] < minArr)
+     {
+        minArr = array[i];
+     }
+  }
+  return minArr;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxArr = array[0];
+  for (let i = 0; i < array.size;i++)
+  {
+     if(array[i] > maxArr)
+     {
+        maxArr = array[i];
+     }
+  }
+  return maxArr;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgSum = 0;
+  for (let i = 0; i < array.size; i++)
+  {
+    avgSum += array[i];
+  }
+  avgSum = avgSum/array.size;
+  return avgSum;
 }
